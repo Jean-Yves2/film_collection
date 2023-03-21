3448ff85374c5c2e16c2da18b4a43aaecc20f1c6
Jean-Yves2 <sarakajean1@gmail.com>
2023-03-21 21:31:16 +0100

    preparation des differents style en fonction des categorie

--- server.js
+++ server.js
@@ -4,20 +4,30 @@ const path=require('path')
 
 const app=express();
 app.set("view engine","ejs")
+app.use(express.static(__dirname+'/public'))
+
+//Part of navigation bar
+const nav_categorie=require('./nav_bar_categorie.json');
 
 
 app.get('/',(req,res)=>{
 
     
-    res.render(path.join(__dirname,'/views/accueil.ejs'),{title:'Welcome'})
+    res.render(path.join(__dirname,'/views/accueil.ejs'),{title:'Welcome' , nav_categorie })
 })
 
-app.get('/horreur',(req,res)=>{ 
+app.get('/:myLink',(req,res)=>{
 
- 
+    const myLink =req.params.myLink.toLowerCase();
+    console.log(myLink)
+    res.locals.switch_css= false;
+
+    if(myLink!='' && myLink !='accueil'){
+        res.locals.switch_css= true;
+    }
     
-    console.log(res.locals.footer)
-    res.render(path.join(__dirname,'/views/accueil.ejs'),{title:'Dark side'})
+    res.render(path.join(__dirname,"/views/"+myLink+".ejs"),{title:myLink , nav_categorie })
+
 })
 
 app.listen(3000)
