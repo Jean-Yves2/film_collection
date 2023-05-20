389ac5545daf05f459f2048717f2d4a9e21de466
Jean-Yves2 <sarakajean1@gmail.com>
2023-05-20 20:39:38 +0200

    :recycle: optimisation de la route acceuil

--- controller/filmController.js
+++ controller/filmController.js
@@ -4,6 +4,9 @@ const nav_categorie=require('../nav_bar_categorie.json');
 const filmController = {
 
     homePage: async (req, res) => {
+
+        const id =req.params.id
+        console.log(id)
         const selectAllFilmOnData = await dataMapper.selectAllFilm();
         const allFilm=selectAllFilmOnData.rows;
         try {
--- router/router.js
+++ router/router.js
@@ -6,8 +6,13 @@ const favorieFilmController=require('../controller/favorieFilmController')
 
 const router = express.Router();
 
-router.get('/',filmController.homePage)
-router.get('/accueil',filmController.homePage)
+router.get('/',(_req,res)=>{
+    res.redirect('/accueil/page/1')
+})
+
+//Pagination
+router.get('/accueil/page/:id',filmController.homePage)
+
 router.get('/favoris',favorieFilmController.favoris)
 
 router.get('/favoris/add/:id',favorieFilmController.addToFavorie)
